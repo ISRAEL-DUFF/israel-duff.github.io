d40df033673a27875e25706ff4e46e6ff2660fb1
Israel Duff <israel-duff@Apples-MacBook-Pro.local>
2025-05-21 17:04:34 +0100

    impleemnting hebrew lex tool

---
+++ vocagam/backend/hebrew/hebrew.service.js
@@ -0,0 +1,150 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const utils = require('./util'); // Import the utils module
+const { parseMorphHB, makeAccentless } = require('./morphParser'); // Import the MorphParser module
+ 
+const hebrewMorphDb = new sqlite3.Database('../data/database/hebrew_morphology.db');
+const hebrewLexiconDb = new sqlite3.Database('../data/database/hebrew_lexicon.db');
+
+function fetchBDBLexiconEntry(strongsNumber) {
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT * FROM bdb_lexicon WHERE strong_number = ?`;
+        hebrewLexiconDb.get(sql, [strongsNumber], (err, row) => {
+            if (err) {
+                console.error('Error fetching lexicon entry:', err);
+                reject(err);
+            } else {
+                if (row) {
+                    // const xmlEntry = row.xml_entry;
+                    // const entry = utils.parseXML(xmlEntry);
+                    resolve(row);
+                } else {
+                    resolve(null);
+                }
+            }
+        });
+    });
+
+    // let query = `SELECT * FROM bdb_lexicon WHERE 1=1`;
+    // const params = [];
+  
+    // if (strongsNumber) {
+    //   query += ` AND strong_number = ?`;
+    //   params.push(strongsNumber);
+    // }
+  
+    // console.log('Query:', query);
+    // console.log('Params:', params);
+  
+    // hebrewLexiconDb.all(query + ` LIMIT 100`, params, (err, rows) => {
+    //   if (err) return res.status(500).json({ error: err.message });
+    //   console.log('Rows:', rows);
+    //   res.json(rows);
+    // });
+}
+
+function fetchStrongsLexiconEntry(strongsNumber) {
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT * FROM strongs_lexicon WHERE strong_number = ?`;
+        hebrewLexiconDb.get(sql, [strongsNumber], (err, row) => {
+            if (err) {
+                console.error('Error fetching lexicon entry:', err);
+                reject(err);
+            } else {
+                if (row) {
+                    resolve({
+                        ...row,
+                        entry: JSON.parse(row.entry)
+                    });
+                } else {
+                    resolve(null);
+                }
+            }
+        });
+    });
+}
+
+async function fetchLexiconEntry(strongsNumber) {
+    const bdbEntry = await fetchBDBLexiconEntry(strongsNumber);
+    const strongsEntry = await fetchStrongsLexiconEntry(strongsNumber);
+    return {
+        bdbEntry,
+        strongsEntry
+    };
+}
+
+async function fetchLexiconEntriesAndMorphology(word) {
+    let query = `SELECT * FROM morphology WHERE 1=1`;
+    return new Promise((resolve, reject) => {
+        const params = [];
+
+        if(!word) {
+            return reject(new Error('Hebrew word missing'))
+        }
+
+        // console.log(utils.normalizeHebrewWord(word), makeAccentless(word))
+        query += ` AND (normalized_word = ? OR strong_number = ?)`;
+        // params.push(utils.normalizeHebrewWord(word));
+        params.push(utils.preprocessWord(word));
+
+        const strongNum = word[0] === 'H' ? word.replace('H', '') : word;
+        params.push(strongNum);
+
+        console.log('Query:', query);
+        console.log('Params:', params);
+        // query + ` LIMIT 100`
+        hebrewMorphDb.all(query + ` LIMIT 100`, params, async (err, rows) => {
+            if (err) return reject(err);
+            // console.log('Rows:', rows);
+            let bookSummary = {}
+
+            for(const row of rows) {
+                if(!bookSummary[row.book]) {
+                    bookSummary[row.book] = {
+                        book_name: row.book,
+                        count: 0,
+                        chapters: {}
+                    }
+                }
+                bookSummary[row.book].count += 1;
+                
+                if(!bookSummary[row.book].chapters[`chapter_${row.chapter}`]) {
+                    bookSummary[row.book].chapters[`chapter_${row.chapter}`] = {
+                        count: 0,
+                        occurrence: []
+                    }
+                }
+                bookSummary[row.book].chapters[`chapter_${row.chapter}`].count += 1;
+                bookSummary[row.book].chapters[`chapter_${row.chapter}`].occurrence.push({
+                    book: row.book,
+                    chapter: row.chapter,
+                    verse: row.verse,
+                    word: row.word,
+                    morphology: parseMorphHB(row.morph),
+                    strongNumber: `H${row.strong_number}`,
+                })
+            }
+
+            resolve({
+                lexicalEntries: rows[0] ? await fetchLexiconEntry("H" + rows[0].strong_number) : undefined,
+                // refs: rows.map(row => ({
+                //     book: row.book,
+                //     chapter: row.chapter,
+                //     verse: row.verse,
+                //     word: row.word,
+                //     morphology: parseMorphHB(row.morph),
+                //     strongNumber: `H${row.strong_number}`,
+                // })),
+                structured: bookSummary,
+                totalOccurrences: rows.length
+            });
+        });
+    })
+}
+
+module.exports = {
+    fetchLexiconEntriesAndMorphology,
+    fetchBDBLexiconEntry,
+    fetchStrongsLexiconEntry,
+    fetchLexiconEntry
+}
--- vocagam/backend/hebrew/server.js
+++ vocagam/backend/hebrew/server.js
@@ -1,125 +1,40 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
-const cors = require('cors'); // Import the CORS middleware
-const utils = require('./util'); // Import the utils module
-const { parseMorphHB, makeAccentless } = require('./morphParser'); // Import the MorphParser module
-
 const app = express();
+const cors = require('cors'); // Import the CORS middleware
+const { fetchLexiconEntriesAndMorphology } = require('./hebrew.service')
+
 // Enable CORS for all routes
 app.use(cors());
- 
-const hebrewMorphDb = new sqlite3.Database('../data/database/hebrew_morphology.db');
-const hebrewLexiconDb = new sqlite3.Database('../data/database/hebrew_lexicon.db');
 
 app.use(express.static('public'));
 app.use(express.json());
 
-function fetchBDBLexiconEntry(strongsNumber) {
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM bdb_lexicon WHERE strong_number = ?`;
-        hebrewLexiconDb.get(sql, [strongsNumber], (err, row) => {
-            if (err) {
-                console.error('Error fetching lexicon entry:', err);
-                reject(err);
-            } else {
-                if (row) {
-                    // const xmlEntry = row.xml_entry;
-                    // const entry = utils.parseXML(xmlEntry);
-                    resolve(row);
-                } else {
-                    resolve(null);
-                }
-            }
-        });
-    });
-
-    // let query = `SELECT * FROM bdb_lexicon WHERE 1=1`;
-    // const params = [];
-  
-    // if (strongsNumber) {
-    //   query += ` AND strong_number = ?`;
-    //   params.push(strongsNumber);
-    // }
-  
-    // console.log('Query:', query);
-    // console.log('Params:', params);
-  
-    // hebrewLexiconDb.all(query + ` LIMIT 100`, params, (err, rows) => {
-    //   if (err) return res.status(500).json({ error: err.message });
-    //   console.log('Rows:', rows);
-    //   res.json(rows);
-    // });
-}
-
-function fetchStrongsLexiconEntry(strongsNumber) {
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM strongs_lexicon WHERE strong_number = ?`;
-        hebrewLexiconDb.get(sql, [strongsNumber], (err, row) => {
-            if (err) {
-                console.error('Error fetching lexicon entry:', err);
-                reject(err);
-            } else {
-                if (row) {
-                    resolve({
-                        ...row,
-                        entry: JSON.parse(row.entry)
-                    });
-                } else {
-                    resolve(null);
-                }
-            }
-        });
-    });
-}
-
-async function fetchLexiconEntry(strongsNumber) {
-    const bdbEntry = await fetchBDBLexiconEntry(strongsNumber);
-    const strongsEntry = await fetchStrongsLexiconEntry(strongsNumber);
-    return {
-        bdbEntry,
-        strongsEntry
-    };
-}
-
-app.post('/search', (req, res) => {
+app.post('/search', async (req, res) => {
   const { word } = req.body;
 
   if(!word) {
     return res.status(400).json({ error: 'Missing word' });
   }
 
-  let query = `SELECT * FROM morphology WHERE 1=1`;
-  const params = [];
+  const data = await fetchLexiconEntriesAndMorphology(word)
 
-  if (word) {
-    // console.log(utils.normalizeHebrewWord(word), makeAccentless(word))
-    query += ` AND (normalized_word = ? OR strong_number = ?)`;
-    // params.push(utils.normalizeHebrewWord(word));
-    params.push(utils.preprocessWord(word));
+  res.send(data)
+  
+});
 
-    const strongNum = word[0] === 'H' ? word.replace('H', '') : word;
-    params.push(strongNum);
-  }
+app.get('/search', async (req, res) => {
+    const { word } = req.query;
 
-  console.log('Query:', query);
-  console.log('Params:', params);
-
-  hebrewMorphDb.all(query + ` LIMIT 100`, params, async (err, rows) => {
-    if (err) return res.status(500).json({ error: err.message });
-    // console.log('Rows:', rows);
-    res.json({
-        lexicalEntries: rows[0] ? await fetchLexiconEntry("H" + rows[0].strong_number) : undefined,
-        refs: rows.map(row => ({
-            book: row.book,
-            chapter: row.chapter,
-            verse: row.verse,
-            word: row.word,
-          morphology: parseMorphHB(row.morph),
-          strongNumber: `H${row.strong_number}`,
-        }))
-    });
-  });
+    console.log(req.query, req.query.word)
+  
+    if(!word) {
+      return res.status(400).json({ error: 'Missing word' });
+    }
+  
+    const data = await fetchLexiconEntriesAndMorphology(word)
+  
+    res.send(data)
+    
 });
 
 app.get('/lexicon', (req, res) => {
